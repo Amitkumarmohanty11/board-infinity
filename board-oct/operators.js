156a4cb0d24778fdb6cbc1d6d0b708d80d118e5e
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2022-11-19 05:38:29 +0530

    JS operators

---
+++ board-oct/operators.js
@@ -0,0 +1,49 @@
+/*Types of JavaScript Operators
+There are different types of JavaScript operators:
+
+1-Arithmetic Operators
+2-Assignment Operators
+3-Comparison Operators
+4-Logical Operators
+5-Conditional Operators
+     Type Operators
+
+//Arithmetic Operators.
+
+     Operator	Description
+    
+     +	         Addition
+     -	         Subtraction
+     *	         Multiplication
+     **        	 Exponentiation (ES2016)
+     /	         Division
+     %	         Modulus (Division Remainder)
+     ++	         Increment
+     --	         Decrement*/
+
+
+
+//"+" Operators (The Addition Operator (+) adds numbers:)
+var x=7;
+var y=7;
+var z=x+y;
+console.log(z);
+
+//"-" Operators
+var a=7;
+var b=7;
+var c=a-b;
+console.log(c);
+
+//The Multiplication Operator (*) multiplies numbers.
+
+
+//Assignment Operators
+/*Operator	    Example	          Same As
+=	             x = y	          x = y
++=	             x += y	          x = x + y
+-=	             x -= y	          x = x - y
+*=	             x *= y	          x = x * y
+/=	             x /= y	          x = x / y
+%=	             x %= y	          x = x % y
+**=          	 x **= y	      x = x ** y*/
