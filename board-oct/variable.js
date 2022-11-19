2a4f2bd8d5f4a4320d74af61243ace315ec02a78
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2022-11-19 07:01:18 +0530

    JS variable

---
+++ board-oct/variable.js
@@ -0,0 +1,24 @@
+/*In Javascript variable are 3 type.
+1.var 
+2.const 
+3.let
+
+Variables are containers for storing data (storing data values).*/
+
+var x=7;
+var y=7;
+var z=x+y;
+console.log(z);
+ 
+
+//const variable 
+//Const value cat not be change after a=amit i catn't  chage the value  a
+const a="amit"
+
+console.log(a)
+
+//let variable
+let g="amit"
+let h="asit"
+g="ashish"
+console.log(g+h)
