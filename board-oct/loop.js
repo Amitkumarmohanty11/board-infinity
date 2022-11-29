aab4871fdaf8e99f7194e464b563ce80ca0ce062
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2022-11-30 00:22:12 +0530

    JS Loops

---
+++ board-oct/loop.js
@@ -0,0 +1,59 @@
+//Loops are 3 type 
+
+/* 1- While loop
+   2- for loop
+   3- do while loop
+*/
+
+
+//While lopp
+//Q..white a program print Number 0 to  5;
+console.log('This is while loop')
+let i=0
+while(i<=5)
+{
+    console.log(i)
+    i++;
+
+    if(i==i)
+    {
+        console.log('amit')
+    }
+}
+
+
+let a=5
+while(a>0)
+{
+    console.log(a)
+    a--;
+}
+
+console.log('This is do while loop')
+// do while loop
+
+
+let x=0
+ do {
+    console.log(x);
+    x++;
+ }
+ while(x<=5)
+
+
+ //For loop 
+
+console.log('This is for loop');/*
+ for(let u=0; u<=5; u++)
+{
+    console.log(u);
+}*/
+ 
+ 
+//let b=0
+for( let b = 0; b < 5; b++)
+{
+    if( b==2) continue 
+   
+    //console.log(i);
+}console.log(i);
