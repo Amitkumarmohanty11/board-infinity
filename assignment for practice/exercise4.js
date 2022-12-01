a4ae6e0cbff1ce37240c826d949df57c61d4991c
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2022-12-01 23:01:07 +0530

    prob-4

---
+++ assignment for practice/exercise4.js
@@ -0,0 +1,28 @@
+/* Q .Write a program to print multiplication table of a given number
+For example, num = 2 so the output should be
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20*/
+
+
+
+
+var num=20;
+let x=''
+
+
+for(let i=1; i<=num;i++)
+{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+}
+
