54a0f11505bc3e1c24882d71fc99ed3191a05aae
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-01-24 20:51:23 +0530

    function Declaration Code

---
+++ board-oct/JavaScript/functions/functiondeclaration.js
@@ -0,0 +1,101 @@
+//Function Declaration 
+
+//Ex:-
+
+
+/*function nameList(name)
+{
+    console.log(name + " How are you");
+}
+
+let name1="Amit";
+let name2="Asit";
+let name3="Ashish";
+let name4="Pratik";
+nameList(name1);
+nameList(name2);
+nameList(name3);
+nameList(name4);*/
+
+
+
+
+
+
+
+/*function nameList(name)
+{
+    let name1= "Butu";//variable Declar in local 
+    console.log(name + " How are you");
+}
+//variable Declar in Global
+let name1="Amit";
+let name2="Asit";
+let name3="Ashish";
+let name4="Pratik";
+nameList(name1);
+nameList(name2);
+nameList(name3);
+nameList(name4);*/
+
+
+
+
+
+
+/*function nameList(name, greetText)
+{
+    console.log(name + " How are you " + greetText);
+}
+
+let name1="Amit";
+let name2="Asit";
+let name3="Ashish";
+let name4="Pratik";
+let greetText="Good morning";
+nameList(name1,greetText);
+nameList(name2,greetText);
+nameList(name3,greetText);
+nameList(name4,greetText);
+*/
+
+
+
+/*function sum(a,b)
+{
+    
+    return a+b;
+    
+}
+
+
+console.log(sum(4,8));
+*/
+
+
+/*function number()
+{
+    
+    for(let i=1; i<=5; i++)
+    {
+        console.log(i);
+    }
+}
+number();*/
+
+
+
+function number()
+{
+    
+    for(let i=5; i>=1; i--)
+    {
+        
+        if(i==2)
+        {
+            return i;
+        }
+        console.log(i);
+    }
+}
+number();
