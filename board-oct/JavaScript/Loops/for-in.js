d04965c75dcdb13bf2aade36a7cbde03fa3378e1
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-02-14 17:33:49 +0530

    JS Loops

---
+++ board-oct/JavaScript/Loops/for-in.js
@@ -0,0 +1,33 @@
+
+/*
+//we can use for in with strings to loop through all the characters.
+
+ myString = "This is my string";
+
+
+for (let char in myString){
+    console.log(myString[char]);
+}
+*/
+
+
+
+/*
+for in loop using object.
+
+ const person={fnme:"amit",lname:"kumar",age:22}
+
+ for (let x in person){
+    console.log(person[x]);
+ }
+ */
+
+
+ 
+ //for in loop using array
+
+ let array =["amit","asit","ashish","pratik"];
+
+ for (let name in array){
+    console.log(name);
+ }
---
+++ board-oct/JavaScript/Loops/for-of.js
@@ -0,0 +1,7 @@
+//for of loop using array
+
+let array =["amit","asit","ashish","pratik"];
+
+for (let name of array){
+   console.log(name);
+}
