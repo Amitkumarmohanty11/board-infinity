9054b7d0d7318ebaf0112460269332dc61195824
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-02-15 23:50:51 +0530

    JS Events

---
+++ board-oct/Advance-Javascript/events.js
@@ -0,0 +1,36 @@
+//What is Events 
+/*
+1.Events are occarences that happens in a system.
+2.System talls as about the events so that we acn react to it.
+3.System is browser in case of javascript.
+4.Events are fired inside browser window.
+
+Example...
+
+1.when a user selects,click or hovers the curson on some element.
+2. user chooses a key on the keyboard
+etc...*/
+
+
+/*console.log("start");
+
+setTimeout(function x()
+{
+    console.log("run")
+},5000);
+//x()
+console.log("end");
+*/
+
+console.log("start");
+
+setTimeout(function x()
+{
+    console.log("run")
+},5000);
+
+
+fetch("https://api.netflix.com").then(function y() {
+    console.log("CB Netflix");
+})
+console.log("end");
