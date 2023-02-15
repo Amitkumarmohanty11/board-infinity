b0ed2b439fcb4c45fecbffc2180365c6e3bef95f
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-02-15 23:52:05 +0530

    JS Callback

---
+++ board-oct/Advance-Javascript/callback.js
@@ -0,0 +1,39 @@
+/* a call back function is a function passed into another function as an argument,
+ which is then invoked inside the outer function to complete an action */
+
+
+ // Asynchronous
+
+ /*Asynchronous action are the action that we initiate now and they finish later.
+
+ eg--setTimeout.
+ 
+    console.log("start");
+
+    setTimeout(function() {
+        console.log("hey I am good");
+    },3000);
+
+    console.log("end");
+
+
+    //output-
+
+    // 1.start
+    // 2.end
+    // 3.hey I am good (after 3 sec print)
+
+    */
+
+
+    //synchronous
+    /* synchronous action are the actios that initiate and finish one by one .
+
+    eg--
+    
+    console.log("start");
+    console.log("amit");
+    console.log("asit");
+    console.log("end");
+
+*/
