62b16f639c31c7e26cb8d066c73cede4cd064f0d
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2023-09-30 04:48:21 +0530

    Basic JS

---
+++ board-oct/JavaScript/Basic-JS/basicjs.js
@@ -0,0 +1,12 @@
+// Q.1 --What is javascript
+
+/* Ans-- JavaScript is a scripting language that enables you to create
+            dynamically updating content, control multimedia, animate images, 
+            and pretty much everything else. (Okay, not everything, but it is amazing
+            what you can achieve with a few lines of JavaScript code.)*/
+
+ // what is ES5 and ES6
+ /* Ans --ES5 is an abbreviation of ECMAScript 5 and also known as ECMAScript 2009.
+          The sixth edition of the ECMAScript standard is ES6 or ECMAScript 6.
+          It is also known as ECMAScript 2015. ES6 is a major enhancement in the JavaScript
+          language that allows us to write programs for complex applications. */
